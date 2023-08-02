e38cea9c48b664a25b37c39c56ff48cae9bb7c9d
Vedanti Bagade <vedanti.r.bagade@gmail.com>
2023-08-02 21:19:55 +0530

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,13 +1,13 @@
 let count = 0
 
 function increment() {
-    count = count + 1
+    count += 1
     document.getElementById("count-el").innerText = count
     console.log(count)
 }
 
 function decrement() {
-    count = count - 1
+    count -= 1
     document.getElementById("count-el").innerText = count
     console.log(count)
 }
